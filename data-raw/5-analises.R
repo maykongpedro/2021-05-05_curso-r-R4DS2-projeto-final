788ed705c86f02ae403cbf0cfa5bac400218da9b
maykongpedro <maykonglaffite@gmail.com>
2021-05-12 23:05:33 -0300

    add análise do mês com maior ocorrência

--- data-raw/5-analises.R
+++ data-raw/5-analises.R
@@ -53,17 +53,56 @@ plot2
 
 
 # Existe algum período do ano onde ocorrem mais crimes? -------------------
-
-ssp_pivot_categorico_tx %>% 
+meses <- 
+    ssp_pivot_categorico_tx %>% 
     dplyr::filter(ano != 2020,
                   pop >= 100000) %>% 
-    #dplyr::mutate(mes_abreviado = month.abb[mes]) %>% 
-    dplyr::mutate(mes_name = month.name[mes]) %>% 
+    #dplyr::mutate(mes_completo = month.name[mes]) %>% 
+    dplyr::mutate(mes_name = month.abb[mes],
+                  mes_name = forcats::fct_relevel(mes_name, month.abb)) %>% 
     dplyr::group_by(mes_name) %>% 
-    dplyr::summarise(total_mil = sum(ocorrencias/1000)) %>% 
-    dplyr::arrange(dplyr::desc(total_mil))
+    dplyr::summarise(total_mil = sum(ocorrencias/1000))
+
+# guardando o mes com maiores ocorrencias
+mes_maior_ocorrencias <- 
+    meses %>% 
+    dplyr::filter(total_mil == max(total_mil))
+
+                
+# plotando meses
+meses %>% 
+    dplyr::mutate(
+        mes_name = forcats::fct_rev(mes_name), # invertendo ordem dos fatores
+        mes_name_label = sprintf("%1.f Mi", round(total_mil, 0)) # criando labels
+    ) %>% 
+    ggplot2::ggplot() +
+    ggplot2::geom_col(ggplot2::aes(x = total_mil, y = mes_name),
+                      fill = ifelse(meses$mes_name != mes_maior_ocorrencias$mes_name,
+                                    "#dfe0df",
+                                    "#443A83")) +
+    ggplot2::scale_x_continuous(expand = ggplot2::expansion(), 
+                                limits = c(0, mes_maior_ocorrencias$total_mil + 100)) +
+    ggplot2::geom_label(
+        ggplot2::aes(
+            x = total_mil,
+            y = mes_name ,
+            label = mes_name_label,
+            hjust = 1.2
+        )) +
+    ggplot2::labs(x = "Taxa de ocorrências por 100 mil habitantes", y = "", 
+                  subtitle = "2002 a 2019 - Apenas cidades com no mínimo 100mil hab.",
+                  caption = "**Dataviz:** @maykongpedro | **Fonte:** SSP (Dados organizados pela Curso-R)") +
+    ggplot2::ggtitle("Mês que apresentou maior taxa de ocorrências de crime no estado de São Paulo") +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(plot.title = ggplot2::element_text(face = "bold", hjust = 0.5),
+                   plot.subtitle = ggplot2::element_text(hjust = 0.5),
+                   axis.line.y = ggplot2::element_line(size = 1),
+                   axis.text.x = ggplot2::element_blank(),
+                   plot.caption = ggtext::element_markdown(hjust = 1),
+                   plot.margin = ggplot2::unit(c(1, 1, 1, 1), "cm"))
 
 
+    
 
 # Quais são as cidades com mais crimes dentro do histórico?
 ssp_pivot_categorico_tx %>% 
