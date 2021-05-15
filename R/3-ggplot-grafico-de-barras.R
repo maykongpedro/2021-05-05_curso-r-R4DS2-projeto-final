2dee3431f3aa98f22cda9d0ee51a3226f265803b
maykongpedro <maykonglaffite@gmail.com>
2021-05-15 18:27:32 -0300

    add função do gráfico de mês

--- R/3-ggplot-grafico-de-barras.R
+++ R/3-ggplot-grafico-de-barras.R
@@ -5,41 +5,43 @@
 #' @param db base de dados
 #' @param x eixo x do gráfico
 #' @param y eixo y do gráfico
-#' @param preenchimento divisão de cores/legendas do gráfico
-#' @param breaks_eixo_x breaks que o eixo x deve ter
+#' @param destaque qual barra deve ser destacada
+#' @param col_destaque coluna que contém os valores que serão usados para destaque
+#' @param rotulos qual barra deve ser destacada
 #' @param titulo título do gráfico
 #' @param subtitulo subtítulo do gráfico
-#' @param tit_legenda título da legenda
+#' @param limite_valor limite dos valores do gráfico
 #' 
 #' @return retorna um gráfico de barras dentro do estilo proposto para a análise
 #' 
 grafico_de_barras <- function(db, 
                               x, 
                               y, 
-                              preenchimento, 
-                              breaks_eixo_x,
+                              destaque,
+                              col_destaque,
+                              rotulos,
                               titulo, 
                               subtitulo, 
-                              tit_legenda) { 
+                              limite_valor) { 
     
     ggplot2::ggplot() +
-        ggplot2::geom_col(ggplot2::aes(x = total_mil, y = mes_name),
-                          fill = ifelse(meses$mes_name != mes_maior_ocorrencias$mes_name,
+        ggplot2::geom_col(data = db, ggplot2::aes(x = {{x}}, y = {{y}}),
+                          fill = ifelse(db[[col_destaque]] != destaque,
                                         "#dfe0df",
                                         "#443A83")) +
         ggplot2::scale_x_continuous(expand = ggplot2::expansion(), 
-                                    limits = c(0, mes_maior_ocorrencias$total_mil + 100)) +
-        ggplot2::geom_label(
+                                    limits = c(0, limite_valor)) +
+        ggplot2::geom_label(data = db,
             ggplot2::aes(
-                x = total_mil,
-                y = mes_name ,
-                label = mes_name_label,
+                x = {{x}},
+                y = {{y}} ,
+                label = {{rotulos}},
                 hjust = 1.2
             )) +
         ggplot2::labs(x = "Taxa de ocorrências por 100 mil habitantes", y = "", 
-                      subtitle = "2002 a 2019 - Apenas cidades com no mínimo 100mil hab.",
+                      subtitle = subtitulo,
                       caption = "**Dataviz:** @maykongpedro | **Fonte:** SSP (Dados organizados pela Curso-R)") +
-        ggplot2::ggtitle("Mês que apresentou maior taxa de ocorrências de crime no estado de São Paulo") +
+        ggplot2::ggtitle(titulo) +
         ggplot2::theme_minimal() +
         ggplot2::theme(plot.title = ggplot2::element_text(face = "bold", hjust = 0.5),
                        plot.subtitle = ggplot2::element_text(hjust = 0.5),
